2daae11ce903967053dc9e5c5fe364a7a5984af9
charankosari <shivacharankosari099@gmail.com>
2025-05-20 20:26:19 +0530

    added s3 utility

--- libs/s3/upload.ts
+++ libs/s3/upload.ts
@@ -1,56 +1,65 @@
-import { ObjectCannedACL, S3Client, type S3ClientConfig } from '@aws-sdk/client-s3';
-import { Upload } from '@aws-sdk/lib-storage';
+import {
+  ObjectCannedACL,
+  S3Client,
+  type S3ClientConfig,
+} from "@aws-sdk/client-s3";
+import { Upload } from "@aws-sdk/lib-storage";
 
-import { config } from '../../config/env';
+import { config } from "../../config/env";
 
 /**
  * Uploader class
  */
 export class Uploader {
-	private static _s3Client: S3Client;
-	private static _s3Opts: { bucket: string };
+  private static _s3Client: S3Client;
+  private static _s3Opts: { bucket: string };
 
-	/**
-	 * Initialize the uploader
-	 * @param bucket Bucket name
-	 */
-	constructor(bucket: string) {
-		const options = {
-			bucket,
-		};
-		Uploader._s3Opts = options;
-		const s3ClientOpts: S3ClientConfig = {
-			region: config.S3_REGION,
-            endpoint: config.S3_ENDPOINT,
-			credentials: {
-				accessKeyId: config.S3_ACCESS_KEY,
-				secretAccessKey: config.S3_SECRET_KEY,
-			},
-		};
-		const client = new S3Client(s3ClientOpts);
-		Uploader._s3Client = client;
-	}
+  /**
+   * Initialize the uploader
+   * @param bucket Bucket name
+   */
+  constructor(bucket: string) {
+    const options = {
+      bucket,
+    };
+    Uploader._s3Opts = options;
+    const s3ClientOpts: S3ClientConfig = {
+      region: config.S3_REGION,
+      endpoint: config.S3_ENDPOINT,
+      credentials: {
+        accessKeyId: config.S3_ACCESS_KEY,
+        secretAccessKey: config.S3_SECRET_KEY,
+      },
+    };
+    const client = new S3Client(s3ClientOpts);
+    Uploader._s3Client = client;
+  }
 
-	/**
-	 * Upload a file to S3
-	 * @param folder_name Location to upload the file
-	 * @param file_name Name of the file
-	 * @param file File to upload
-	 * @param acl `public-read` or `private` access
-	 */
-	async uploadFile(folder_name: string, file_name: string, file: File, acl: ObjectCannedACL) {
-		const parallelUploads3 = new Upload({
-			client: Uploader._s3Client,
-			params: {
-				Bucket: Uploader._s3Opts.bucket,
-				ACL: acl,
-				Body: file,
-				Key: folder_name + '/' + file_name,
-			},
-		});
+  /**
+   * Upload a file to S3
+   * @param folder_name Location to upload the file
+   * @param file_name Name of the file
+   * @param file File to upload
+   * @param acl `public-read` or `private` access
+   */
+  async uploadFile(
+    folder_name: string,
+    file_name: string,
+    file: File
+    // acl: ObjectCannedACL
+  ) {
+    const parallelUploads3 = new Upload({
+      client: Uploader._s3Client,
+      params: {
+        Bucket: Uploader._s3Opts.bucket,
+        // ACL: acl,
+        Body: file,
+        Key: folder_name + "/" + file_name,
+      },
+    });
 
-		await parallelUploads3.done();
+    await parallelUploads3.done();
 
-		return `${config.S3_URL}/${folder_name}/${file_name}`;
-	}
+    return `${config.S3_URL}/${folder_name}/${file_name}`;
+  }
 }
--- routes/auth.route.ts
+++ routes/auth.route.ts
@@ -5,14 +5,15 @@ import { authMiddleware } from "../middlewares/auth.middleware";
 
 const authRoute = new Hono();
 
-authRoute.post('/login-request', AuthController.requestAccountAccess);
+authRoute.post("/login-request", AuthController.requestAccountAccess);
 // Add support for Email based login
-authRoute.post('/verify-account-access', AuthController.verifyAccountAccess);
-
+authRoute.post("/verify-account-access", AuthController.verifyAccountAccess);
+authRoute.post("/user/email", AuthController.setEmailForVerification);
+authRoute.post("/user/verify-email", AuthController.verifyEmail);
 // TODO: Add Web Account Creation Route
 authRoute.use(authMiddleware());
 
-authRoute.get('/user', AuthController.getUser);
-authRoute.patch('/user', AuthController.updateUser);
+authRoute.get("/user", AuthController.getUser);
+authRoute.patch("/user", AuthController.updateUser);
 
 export { authRoute };
